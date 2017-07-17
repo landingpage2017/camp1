682b844f00af1b723f59e3ebdf4117e5266fccdb
landingpage2017 <2122238@gmail.com>
2017-07-18 01:28:46 +0200

    Add files via upload

--- js/sendform.js
+++ js/sendform.js
@@ -57,9 +57,10 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
     xhr.onreadystatechange = function() {
         console.log( xhr.status, xhr.statusText )
         console.log(xhr.responseText);
-        document.getElementById('gform').style.display = 'none'; // hide form
-        document.getElementById('formtitle').style.display = 'none';
-        document.getElementById('thankyou_message').style.display = 'block';
+        window.location.href = "./nir-yitzhak-thanks.html";
+        // document.getElementById('gform').style.display = 'none'; // hide form
+        // document.getElementById('formtitle').style.display = 'none';
+        // document.getElementById('thankyou_message').style.display = 'block';
         return;
     };
     // url encode form data for sending as post data
